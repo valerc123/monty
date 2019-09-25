0767490683a98644913c78807c162f8203328171
valerc123 <valentina.rua123@gmail.com>
2019-09-25 22:45:28 +0000

    Add comments, betty fixed

--- parse_line.c
+++ parse_line.c
@@ -1,9 +1,14 @@
 #include "monty.h"
+/**
+* parse_line - Parses a line in to tokens, only get two first tokens
+* @line: string to tokenize
+* Return: double pointer of tokens, or NULL if failed
+*/
 char **parse_line(char *line)
 {
 	char *token;
 	char **tokens;
-	int i;
+	unsigned int i;
 
 	tokens = malloc(sizeof(char *) * 3);
 	if (tokens == NULL)
@@ -18,7 +23,7 @@ char **parse_line(char *line)
 		return (NULL);
 	}
 	i = 0;
-	while (token != NULL )
+	while (token != NULL && i < 2)
 	{
 		tokens[i] = token;
 		token = strtok(NULL, " '\n'");
