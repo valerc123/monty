a69031af2f04c4f4e851f5aee8997bcc7b81f63c
apatinoc4 <apatinoc4@gmail.com>
2019-09-26 18:09:32 +0000

    more updates

--- monty.h
+++ monty.h
@@ -51,4 +51,5 @@ void op_pint(stack_t **stack, unsigned int line_number);
 void op_swap(stack_t **stack, unsigned int line_number);
 void free_list(stack_t **stack);
 void op_nop(stack_t **stack, unsigned int line_number);
+void error_func(unsigned int line_number, int error_number);
 #endif
--- more_stack_functions.c
+++ more_stack_functions.c
@@ -23,3 +23,31 @@ void op_pint(stack_t **stack, unsigned int line_number)
 		error_func(line_number, 2);
 	printf("%d\n", (*stack)->n);
 }
+
+/**
+ * op_pop - Removes stacks top element
+ * @stack: our stack
+ * @line_number: instruction number
+ * Return: void
+ */
+void op_pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr;
+
+	if (*stack == NULL)
+		error_func(line_number, 3);
+
+	if ((*stack)->next == NULL)
+	{
+		free(*stack);
+		*stack = NULL;
+	}
+	else
+	{
+		ptr = (*stack)->next;
+		(*stack) = ptr;
+		ptr = ptr->prev;
+		(*stack)->prev = NULL;
+		free(ptr);
+	}
+}
