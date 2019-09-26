78d6d6841cd2ce01dd761ba86782ef7ddef158c4
valerc123 <valentina.rua123@gmail.com>
2019-09-26 21:11:15 +0000

    fixed sintax

--- stack_functions.c
+++ stack_functions.c
@@ -36,7 +36,7 @@ void op_push(stack_t **stack, unsigned int line_number)
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
-		error_func("usage: push integer", line_number);
+		dprintf(STDOUT_FILENO, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 	new_node->n = arg;
