7a8a2d45e1ac410e3897bf1361fb90b2f5a4b563
apatinoc4 <apatinoc4@gmail.com>
2019-09-26 03:42:40 +0000

    updated header function

--- monty.h
+++ monty.h
@@ -1,11 +1,13 @@
-#ifndef MONTY_H_
-
-#define MONTY_H_
-
-#include <stdio.h>
+#ifndef MONTY_H
+#define MONTY_H
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -17,13 +19,12 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
-
 /**
- * struct instruction_s - opcode and its function
+ * struct instruction_s - opcoode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
@@ -32,10 +33,19 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+extern int arg;
 char **parse_line(char *line);
-
+int valid_op(char **tokens);
+int valid_arg(char *tokens);
+void (*get_function(char **, unsigned int))(stack_t **, unsigned int);
+void op_push(stack_t **stack, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+void free_list(stack_t **stack);
 #endif
